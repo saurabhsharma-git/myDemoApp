1b972ed9ab8f23e0ef6c404bb51aa454656f04f7
saurabhsharma-git <saurabhks88@gmail.com>
2026-06-05 14:28:43 +0530

    Logging and Exception Added

---
+++ demo.py
@@ -0,0 +1,17 @@
+# from src.logger import logging
+
+# logging.debug("This is a debug message")
+# logging.info("This is an info message")
+# logging.warning("This is a warning message")
+# logging.error("This is an error message")
+# logging.critical("This is a critical message")
+
+# from src.exception import CustomException
+# import sys
+# from src.logger import logging
+
+# try:
+#     a = 1 / 0
+# except Exception as e:  
+#     logging.info("We are dividing by zero")
+#     raise CustomException(e, sys) from e  
---
+++ src/logger/__init__.py
@@ -0,0 +1,35 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+from from_root import from_root
+
+LOG_DIR = 'logs'
+LOG_FILE = f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}.log'
+MAX_LOG_SIZE = 10 * 1024 * 1024  # 10 MB
+BACKUP_COUNT = 3
+
+log_dir_path = os.path.join(from_root(), LOG_DIR)
+os.makedirs(log_dir_path, exist_ok=True)
+log_file_path = os.path.join(log_dir_path, LOG_FILE)
+
+def configure_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(console_formatter)
+
+    # File handler with rotation
+    file_handler = RotatingFileHandler(log_file_path, maxBytes=MAX_LOG_SIZE, backupCount=BACKUP_COUNT)
+    file_handler.setLevel(logging.DEBUG)
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+configure_logger()
