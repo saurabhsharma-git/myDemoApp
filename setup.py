0c641187a94f06f730ce1c46c397acd83d61df55
saurabhsharma-git <saurabhks88@gmail.com>
2026-06-05 12:48:50 +0530

    Project Structure Ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.1.0',
+    author='Saurabh Sharma',
+    author_email='skmr19@gmail.com',
+    packages=find_packages()
+    )
